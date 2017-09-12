65d6ee09b91653f170ca19af4d94747492099d3a
HJW <jw9903@naver.com>
2017-09-12 12:55:30 +0900

    hello

---
+++ app/src/main/java/com/example/hywoman/sep12/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.example.hywoman.sep12;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void copy (View v){
+        EditText aa = (EditText) findViewById(R.id.editText);
+        //aa.getText();
+
+        TextView bb = (TextView) findViewById(R.id.textView);
+        bb.setText( aa.getText());
+    }
+}
---
+++ app/src/main/java/com/example/hywoman/sep12/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.hywoman.sep12;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void NextScreen ( View v) {
+
+        Intent i = new Intent(this , Main2Activity.class);
+        startActivity(i);
+
+        Toast toast = Toast.makeText(getApplicationContext(), "hello", Toast.LENGTH_LONG);
+        toast.show();
+
+
+    }
+
+
+}
